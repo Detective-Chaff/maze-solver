983c97d2b32fa020256337e4575fadff1af965ea
Detective-Chaff <toddoneil42@gmail.com>
2025-04-07 18:47:42 -0400

    added draw_move method to Cell class, which is responsible for drawing the traversal lines through each cell

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from src.classes.cell import Cell
-from src.classes.line import Line
-from src.classes.point import Point
 from src.classes.window import Window
 
 
 def main():
     print("starting")
     win = Window(800, 600)
-    point_a = Point(200, 150)
-    point_b = Point(600, 450)
-    cell_a = Cell(win)
+    #left
+    c = Cell(win)
+    # c.left_wall = False
+    c.draw(50, 50, 100, 100)
+    a = c
+    #center left
+    c = Cell(win)
+    # c.right_wall = False
+    c.draw(50, 100, 100, 150)
+    #center right
+    c = Cell(win)
+    # c.bottom_wall = False
+    c.draw(100, 50, 150, 100)
+    b = c
+    #right
+    c = Cell(win)
+    # c.top_wall = False
+    c.draw(100, 100, 150, 150)
+    a.draw_move(b)
 
-    cell_a.left_wall = False
-    cell_a.bottom_wall = False
-    cell_a.draw(point_a.x, point_a.y, point_b.x, point_b.y)
-    # line = Line(point_a, point_b)
-    # win.draw_line(line, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
--- src/classes/cell.py
+++ src/classes/cell.py
@@ -16,24 +16,42 @@ class Cell():
         self._win = window
     
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
 
         if self.left_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line, "black")
         if self.right_wall:
-            line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line, "black")
             # self._win._canvas.create_line(x2, y1, x2, y2)
         if  self.top_wall:
-            line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line, "black")
             # self._win._canvas.create_line(x1, y1, x2, y1)
         if self.bottom_wall:
-            line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line, "black")
             # self._win._canvas.create_line(x1, y2, x2, y2)
             
+    def draw_move(self, to_cell, undo=False):
+        colour = "red"
+        if undo:
+            colour = "gray"
+
+        center_y = (self._y1 + self._y2) // 2
+        center_x = (self._x1 + self._x2) // 2
+        p1 = Point(center_x, center_y)
+            
+        center_y = (to_cell._y1 + to_cell._y2) // 2
+        center_x = (to_cell._x1 + to_cell._x2) // 2
+        p2 = Point(center_x, center_y)
+            
+        cl = Line(p1,p2)
+        self._win.draw_line(cl, colour)
